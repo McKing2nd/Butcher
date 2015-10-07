552ed19ee3cbb86089d7b28f77f5b51c84cc1e6d
vince <v.mecking@gmail.com>
2015-10-07 20:54:56 +0200

    Added tiles!

--- Butcher/src/nl/mecking/butcher/graphics/Screen.java
+++ Butcher/src/nl/mecking/butcher/graphics/Screen.java
@@ -1,14 +1,44 @@
 package nl.mecking.butcher.graphics;
 
+import java.util.Random;
+
 public class Screen {
 
 	private int width, height;
 	public int[] pixels;
 	
+	public int[] tiles = new int[64*64];
+	
+	private Random random = new Random();
+	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width*height];
+		
+		for (int i=0; i< 64 * 64; i++) {
+			tiles[i] = random.nextInt(0xffffff);
+		}
+	}
+	
+	/*
+	 * Clear makes all pixels in the array black
+	 */
+	public void clear() {
+		for(int i=0; i < pixels.length; i++) {
+			pixels[i] = 0;
+		}
+	}
+	
+	public void render() {
+		for(int y=0; y < height; y++) {
+			if(y < 0 || y >= height) break;
+			for(int x=0; x<width; x++) {
+				if(x < 0 || x >= width) break;
+				int tileIndex = (x >> 4) + (y >> 4) * 64; // >> 4 is the same as /16
+				pixels[x + y * width] = tiles[tileIndex];
+			}
+		}
 	}
 	
 }
