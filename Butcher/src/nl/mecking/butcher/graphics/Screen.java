53dacad8acfa1bcd275bf702c50d1e4467067429
Vince <v.mecking@gmail.com>
2015-10-08 22:12:33 +0200

    fixed some variables

--- Butcher/src/nl/mecking/butcher/graphics/Screen.java
+++ Butcher/src/nl/mecking/butcher/graphics/Screen.java
@@ -6,43 +6,48 @@ public class Screen {
 
 	private int width, height;
 	public int[] pixels;
-	
-	public int[] tiles = new int[64*64];
-	
+	public final int MAP_SIZE = 64;
+	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+
+	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
+
 	private Random random = new Random();
-	
+
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		// Array with every pixel on the game screen
-		pixels = new int[width*height];
-		
+		pixels = new int[width * height];
+
 		/*
 		 * Set up colors for the tiles
 		 */
-		for (int i=0; i< 64 * 64; i++) {
+		for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
 			tiles[i] = random.nextInt(0xffffff);
 		}
 	}
-	
+
 	/*
 	 * Clear makes all pixels in the array black
 	 */
 	public void clear() {
-		for(int i=0; i < pixels.length; i++) {
+		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = 0;
 		}
 	}
-	
+
 	public void render() {
-		for(int y=0; y < height; y++) {
-			if(y < 0 || y >= height) break;
-			for(int x=0; x<width; x++) {
-				if(x < 0 || x >= width) break;
-				int tileIndex = (x >> 4) + (y >> 4) * 64; // >> 4 is the same as /16
+		for (int y = 0; y < height; y++) {
+			if (y < 0 || y >= height)
+				break;
+			for (int x = 0; x < width; x++) {
+				if (x < 0 || x >= width)
+					break;
+				int tileIndex = (x >> 4) + (y >> 4) * MAP_SIZE; // >> 4 is the same as
+															// /16
 				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
 	}
-	
+
 }
