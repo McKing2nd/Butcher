d886ac9c6edb9486e73f74066bc75f8799c02feb
Vince <v.mecking@gmail.com>
2015-10-09 23:26:45 +0200

    Clean up Screen code (random colored boxes)

--- Butcher/src/nl/mecking/butcher/graphics/Screen.java
+++ Butcher/src/nl/mecking/butcher/graphics/Screen.java
@@ -1,7 +1,5 @@
 package nl.mecking.butcher.graphics;
 
-import java.util.Random;
-
 public class Screen {
 
 	private int width, height;
@@ -9,22 +7,12 @@ public class Screen {
 	public final int MAP_SIZE = 64;
 	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 
-	private int[] tiles = new int[MAP_SIZE * MAP_SIZE];
-
-	private Random random = new Random();
-
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		// Array with every pixel on the game screen
 		pixels = new int[width * height];
 
-		/*
-		 * Set up colors for the tiles
-		 */
-		for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
-			tiles[i] = random.nextInt(0xffffff);
-		}
 	}
 
 	/*
@@ -43,7 +31,6 @@ public class Screen {
 			for (int x = 0; x < width; x++) {
 				if (x < 0 || x >= width)
 					break;
-				int tileIndex = ((x + xOffset >> 4) & MAP_SIZE_MASK) + ((y + yOffset >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
 				pixels[x + y * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
 			}
 		}
